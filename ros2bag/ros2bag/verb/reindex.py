ab7e696e66373556992227dd1d82e97c1b725049
Jacob Hassold <jhassold@dcscorp.com>
2020-10-21 14:35:25 -0400

    Fix more licenses Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- ros2bag/ros2bag/verb/reindex.py
+++ ros2bag/ros2bag/verb/reindex.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Open Source Robotics Foundation, Inc.
+# Copyright 2020 DCS Corporation, All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# DISTRIBUTION A. Approved for public release; distribution unlimited.
+# OPSEC #4584.
+#
+# Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
+# Part 252.227-7013 or 7014 (Feb 2014).
+#
+# This notice must appear in all copies of this file and its derivatives.
 
 import os
 
